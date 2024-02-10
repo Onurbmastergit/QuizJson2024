ac4a1dc4d0fe0d1b316aff5ea1f8741a8d598394
brunoathayde <142446897+Onurbmastergit@users.noreply.github.com>
2024-02-09 23:48:20 -0300

    version with case 2.0

---
+++ Assets/Scripts/TestCase.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class TestCase : MonoBehaviour
+{
+    public GameObject painelWin;
+    public GameObject painelOver;
+    public GameObject caso;
+
+    public void RepostaCerta() // Agora é público
+    {
+        caso.SetActive(false);
+        painelWin.SetActive(true);
+    }
+
+    public void RepostaErrada() // Agora é público
+    {
+        caso.SetActive(false);
+        painelOver.SetActive(true);
+    }
+
+    public void BackToMenu() // Agora é público
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
